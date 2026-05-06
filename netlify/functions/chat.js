9f559fb6f38d5bddca38d85b0df253eba4ceb744
Rohan S Bhat <76154331+R451748@users.noreply.github.com>
2026-05-06 15:58:54 +0000

    Updated Gemini direct chat

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -1,11 +1,27 @@
 exports.handler = async function(event) {
 
+  if (event.httpMethod !== "POST") {
+
+    return {
+
+      statusCode: 405,
+
+      body: JSON.stringify({
+        reply: "Method Not Allowed"
+      })
+
+    };
+
+  }
+
   try {
 
     const { message } = JSON.parse(event.body);
 
     const response = await fetch(
+
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+
       {
 
         method: "POST",
@@ -24,20 +40,8 @@ exports.handler = async function(event) {
 
                 {
 
-                  text: `
-You are RohanAI assistant.
+                  text: message
 
-Answer only about:
-- Rohan's skills
-- projects
-- education
-- internships
-- resume
-- contact
-
-User Question:
-${message}
-                  `
                 }
 
               ]
@@ -49,20 +53,21 @@ ${message}
         })
 
       }
+
     );
 
     const data = await response.json();
 
     console.log(data);
 
-    if (!data.candidates) {
+    if (data.error) {
 
       return {
 
         statusCode: 500,
 
         body: JSON.stringify({
-          reply: "Gemini API error."
+          reply: data.error.message
         })
 
       };
