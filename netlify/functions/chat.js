f4205d4bb8d89d95a949ac0767b897f11723d125
Rohan S Bhat <76154331+R451748@users.noreply.github.com>
2026-05-06 18:00:50 +0000

    Added real resume AI

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -26,7 +26,7 @@ exports.handler = async function(event) {
     // Read Resume PDF
     const pdfPath = path.join(
       process.cwd(),
-      "/workspaces/MyPortfolio/Resume__Copy___Copy_.pdf"
+      "Resume__Copy___Copy_.pdf"
     );
 
     const pdfBuffer = fs.readFileSync(pdfPath);
